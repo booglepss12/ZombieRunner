057a626e6efb1e0527e3e7ca0d4a651ef8d79ace
Unknown <grooney4@gmail.com>
2019-07-17 15:06:11 -0400

    Initial Commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
+    bool isProvoked = false;
+    void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (isProvoked){
+            EngageEnemy();
+        }else if (distanceToTarget <= chaseRange){
+
+            isProvoked = true;
+           
+        }
+
+    }
+
+    private void EngageEnemy()
+    {
+        if(distanceToTarget >= navMeshAgent.stoppingDistance)
+        {
+            ChaseTarget();
+        }
+        if (distanceToTarget <= navMeshAgent.stoppingDistance)
+        {
+            AttackEnemy();
+        }
+    }
+
+	private void ChaseTarget()
+    {
+        navMeshAgent.SetDestination(target.position);
+    }
+
+    private void AttackEnemy()
+    {
+        Debug.Log(name + " has begun to attack! Exterminate!");
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, chaseRange);
+	}
+}
